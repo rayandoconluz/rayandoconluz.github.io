e863c6f9dcdf26ae4ef977034452f2b8d443ee62
Juan Gabriel <tuxgabo@gmail.com>
2018-03-11 15:49:53 -0500

    pinteres

--- _javascript/components/shell.js
+++ _javascript/components/shell.js
@@ -75,13 +75,13 @@ export default class Shell extends React.Component {
               <div className="side-bar__mask" onClick={() => { this.toggleSideBar(false) }} >
             </div>      
         }      
-        <div className="header" style={{ backgroundColor: documents.jekyll.brandColor, borderBottom: "solid 1px black" }} >
-          <div className='container'>
-            <Link to='/'>
+        <header className="site-header" style={{ backgroundColor: documents.jekyll.brandColor, borderBottom: "solid 1px black" }} >
+          <div className='header-wrapper page-content'>
+            <Link className="site-logo" to='/'>
               <img src={documents.jekyll.logo} height="50" width="50">
               </img>
             </Link>        
-            <button onClick={() => { this.toggleSideBar(true) }} className='menu'>      
+            <button onClick={() => { this.toggleSideBar(true) }} className='ampstart-btn caps m2 menu-button'>      
                <svg viewBox="0 0 18 15" width="35px" height="45px">
                 <path fill="#424242" d="M18,1.484c0,0.82-0.665,1.484-1.484,1.484H1.484C0.665,2.969,0,2.304,0,1.484l0,0C0,0.665,0.665,0,1.484,0 h15.031C17.335,0,18,0.665,18,1.484L18,1.484z"/>
                 <path fill="#424242" d="M18,7.516C18,8.335,17.335,9,16.516,9H1.484C0.665,9,0,8.335,0,7.516l0,0c0-0.82,0.665-1.484,1.484-1.484 h15.031C17.335,6.031,18,6.696,18,7.516L18,7.516z"/>
@@ -89,7 +89,7 @@ export default class Shell extends React.Component {
                 </svg>              
             </button>
          </div>
-        </div>
+        </header>
         <div className='container main'>
           <ReactTransitionGroup>
             {
