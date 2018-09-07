d686e34cef479baab18dd023ac0840e98a5e7a39
Juan Gabriel <tuxgabo@gmail.com>
2018-09-07 10:45:59 -0500

    new sw now we are over https :)

--- service-worker.js
+++ service-worker.js
@@ -1,86 +1,38 @@
-var cache_name = "rutas-cache-v112";
+var cache_name = "rutas-cache-v210";
 var urlsToCache = [
-  "/",
   "/images/logo.png",
-  "/images/loading.gif",
-  "/documents.json",
-  "/javascript/index.bundle.js",
-  "https://cdn.ampproject.org/v0.js",
-  "https://cdn.ampproject.org/v0/amp-iframe-0.1.js",
-  "https://cdn.ampproject.org/v0/amp-carousel-0.1.js",
-  "https://cdn.ampproject.org/v0/amp-analytics-0.1.js",
-  "https://cdn.ampproject.org/v0/amp-video-0.1.js",
-  "https://cdn.ampproject.org/v0/amp-sidebar-0.1.js",
-  "https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js",
-  "https://cdn.ampproject.org/shadow-v0.js",
-  "https://cdn.ampproject.org/v0/amp-social-share-0.1.js"
+  "/images/loading.gif"
 ];
-/*// Any requests that match the regular expression will match this route, with
-// the capture groups passed along to the handler as an array via params.
-const workboxSW = new WorkboxSW();
 
-workboxSW.precache([
-  {
-    url: '/index.html',
-    revision: 'cb13ex',
-  }, {
-    url: '/javascript/index.bundle.js',
-    revision: 'b33xqw',
-  },
-  {
-    url: '/documents.json',
-    revision: 'c34xbc',
-  }
-]);
-*/
-/*
-// Register runtime routes like so.
-workboxSW.router.registerRoute(
-  /\/images\/(.*\/)?.*\.(png|jpg|jpeg|gif)/,
-  workboxSW.strategies.cacheFirst());
-*/
-self.addEventListener("install", function(event) {
-  // Perform install steps
-  event.waitUntil(fetch("/documents.json").
-    then(documents => documents.json()).
-    then((documents) => documents.docs).
-    then((jsonDocuments) => jsonDocuments.reduce((urlsToCache, document) => urlsToCache.concat(document.url, document.image), urlsToCache)).
-    then((imagesArray) => { 
-      caches.open(cache_name).then(function(cache) {
-      return cache.addAll(urlsToCache.concat(imagesArray));})
-    })
-  );
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(cache_name).then((cache) => cache.addAll(urlsToCache))
+  )
 });
+
 self.addEventListener("push", event => {
   if (event.data) {
-    var img = "/images/launcher-icon-4x.png";
     // star wars vibration pattern
+    let data = event.data.json()
     const options = {
-      body: event.data.text(),
-      vibrate: [
-        500,
-        110,
-        500,
-        110,
-        450,
-        110,
-        200,
-        110,
-        170,
-        40,
-        450,
-        110,
-        200,
-        110,
-        170,
-        40,
-        500
-      ],
-      icon: img,
-      badge: img
+      body: data.body,
+      vibrate:data.vibrate,
+      icon: data.icon,
+      image: data.image,
+      badge: data.badge,
+      requireInteraction: true,
+      data: {
+        time: new Date(Date.now()).toString(),
+        id: data.id,
+        url: data.url
+      }
     };
+    if (data.actions) { 
+      options.actions = data.actions
+    }
     const promiseChain = self.registration.showNotification(
-      "Notificacion de Rutas",
+      data.title,
       options
     );
     event.waitUntil(promiseChain);
@@ -88,17 +40,39 @@ self.addEventListener("push", event => {
     console.log("this push event has not data");
   }
 });
-self.addEventListener("fetch", function(event) {
+
+self.addEventListener('notificationclick', function (event) {
+  const clickedNotification = event.notification;
+  const notificationData = clickedNotification.data;
+  clickedNotification.close();
+  if (!event.action) {
+    //data sent from server
+    const page = notificationData.url;
+    const promiseChain = clients.openWindow(page);
+    event.waitUntil(promiseChain);
+    return;
+  }
+  else { 
+    const pushInfoPromise = fetch(`/api/actions/${event.action}/${notificationData.id}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      const urlAction = response.url;
+      return clients.openWindow(urlAction)
+    });
+    event.waitUntil(pushInfoPromise)
+  }
+});
+
+self.addEventListener("fetch", function (event) {
+  // Prevent the default, and handle the request ourselves.
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
+      return response || fetch(event.request);
     })
   );
-});
+})
 
 self.addEventListener("activate", function(event) {
   event.waitUntil(
